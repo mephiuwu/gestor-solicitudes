4c29b5fb946c6beea8504cc41f5c8cee769418ae
ramirosaezsa <ramirosaezsa@gmail.com>
2025-11-27 19:42:57 -0300

    feat(solicitudes): implementar métodos de almacenamiento y actualización con validaciones

--- gestor-solicitudes-api/app/Http/Controllers/SolicitudController.php
+++ gestor-solicitudes-api/app/Http/Controllers/SolicitudController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreSolicitudRequest;
+use App\Http\Requests\UpdateSolicitudEstadoRequest;
+use App\Http\Resources\SolicitudResource;
+use App\Models\Solicitud;
 use Illuminate\Http\Request;
 
 class SolicitudController extends Controller
 {
     public function index() {
+        $solicitudes = Solicitud::orderBy('id', 'desc')->get();
 
+        return SolicitudResource::collection($solicitudes);
     }
 
-    public function store() {
+    public function store(StoreSolicitudRequest $request) {
+        $solicitud = Solicitud::create([
+            'nombre_documento' => $request->nombre_documento,
+            'estado' => 'pendiente',
+            'fecha_creacion' => now(),
+        ]);
 
+        return new SolicitudResource($solicitud);
     }
 
-    public function update() {
-        
+    public function update(UpdateSolicitudEstadoRequest $request, $id) {
+        $solicitud = Solicitud::findOrFail($id);
+
+        $solicitud->update([
+            'estado' => $request->estado
+        ]);
+
+        return new SolicitudResource($solicitud);
     }
 }
